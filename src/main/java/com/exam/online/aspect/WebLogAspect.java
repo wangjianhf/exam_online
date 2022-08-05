19d2d1bb26a256f53a1c528048ddee94d37d894d
“wangjian” <wangjian_hf@163.com>
2022-08-05 17:51:26 +0800

    删除log 文件

--- src/main/java/com/exam/online/aspect/WebLogAspect.java
+++ src/main/java/com/exam/online/aspect/WebLogAspect.java
@@ -13,18 +13,24 @@ import java.util.Arrays;
 
 
 /**
- * 请求日志处理
+ * AOP 记录 请求处理日志
  */
 @Aspect
 @Component
 public class WebLogAspect {
     private static final Logger LOGGER = LoggerFactory.getLogger(WebLogAspect.class);
 
+    private static final String REQUEST_PREFIX = "==================>>>>>>>>>>>>>  ";
+    private static final String REQUEST_SUFFIX = " >>>>>>>>>>>>>==================";
+    private static final String RESPONSE_PREFIX = "==================<<<<<<<<<<<<< ";
+    private static final String RESPONSE_SUFFIX = " <<<<<<<<<<<<<==================";
+
     ThreadLocal<Long> startTime = new ThreadLocal<>();
 
     // 切入点
     @Pointcut("execution(* com.exam.online.controller..*.*(..))")
-    public void pc() {}
+    public void pc() {
+    }
 
     // 前置通知
     @Before("pc()")
@@ -35,20 +41,26 @@ public class WebLogAspect {
         // 接收请求, 记录请求内容
         ServletRequestAttributes requestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
         HttpServletRequest request = requestAttributes.getRequest();
+
         // 记录请求内容
-        LOGGER.info("URL: " + request.getRequestURL().toString());
+        LOGGER.info(REQUEST_PREFIX + "REQUEST AOP START" + REQUEST_SUFFIX);
+        LOGGER.info("API_URL: " + request.getRequestURL().toString());
         LOGGER.info("HTTP_METHOD: " + request.getMethod());
         LOGGER.info("IP: " + request.getRemoteAddr());
         LOGGER.info("CLASS_METHOD: " + joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName());
-        LOGGER.info("Args: " + Arrays.toString(joinPoint.getArgs()));
+        LOGGER.info("REQUEST BODY: " + Arrays.toString(joinPoint.getArgs()));
+        LOGGER.info(REQUEST_PREFIX + "REQUEST AOP OVER " + REQUEST_SUFFIX);
     }
 
     // 返回通知
     @AfterReturning(returning = "result", pointcut = "pc()")
     public void doAfterReturning(Object result) {
+
         // 请求返回内容
-        LOGGER.info("RESPONSE: " + result);
+        LOGGER.info(RESPONSE_PREFIX + "RESPONSE AOP START" + RESPONSE_SUFFIX);
+        LOGGER.info("RESPONSE BODY: " + result);
         LOGGER.info("SPEND TIME: " + (System.currentTimeMillis() - startTime.get()));
+        LOGGER.info(RESPONSE_PREFIX + "RESPONSE AOP OVER " + RESPONSE_SUFFIX);
 
         // 用完之后移除, 避免内存泄漏
         startTime.remove();
