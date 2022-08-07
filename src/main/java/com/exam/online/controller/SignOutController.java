ed05ff15034bb9793f4e510d442e2ac83baf0162
wangjian <wangjian_hf@163.com>
2022-08-07 23:18:43 +0800

    登录功能改造

---
+++ src/main/java/com/exam/online/controller/SignOutController.java
@@ -0,0 +1,10 @@
+package com.exam.online.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/sign_out")
+public class SignOutController {
+
+}
